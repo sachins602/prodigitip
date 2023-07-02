ada42bf84815bfe7ceadeee29c0e01c0c7ef6c13
sachins602 <sachinsapkota4@gmail.com>
2023-07-03 02:42:55 +0545

    add blog type and schema

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,3 +4,24 @@ export type ContactInput = {
   subject: string;
   description: string;
 };
+
+export type BlogType = {
+  id: number;
+  author: string;
+  authorTitle: string;
+  publishedOn: string;
+  authorImage: string;
+  title: string;
+  paragraph1: string;
+  image1: string;
+  paragraph2: string;
+  paragraph3: string;
+  paragraph4: string;
+  paragraph5: string;
+  image2: string;
+  paragraph6: string;
+  paragraph7: string;
+  paragraph8: string;
+  image3: string;
+  paragraph9: string;
+};
