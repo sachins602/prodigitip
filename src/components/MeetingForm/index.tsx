3ac9928cba73bd9fa69e0c84e82ebf4f257b0155
sachins602 <sachinsapkota4@gmail.com>
2023-07-02 16:03:06 +0545

    add helper function to remove type error and close popover on day click

--- src/components/MeetingForm/index.tsx
+++ src/components/MeetingForm/index.tsx
@@ -20,18 +20,29 @@ import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 
 const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  fullname: z.string().min(2, {
+    message: "Full Name must be at least 2 characters.",
+  }),
+  date: z.date().min(new Date(), {
+    message: "Date must be in the future.",
   }),
 });
 
+function wrapAsyncFunction<ARGS extends unknown[]>(
+  fn: (...args: ARGS) => Promise<unknown>
+): (...args: ARGS) => void {
+  return (...args) => {
+    void fn(...args);
+  };
+}
+
 export function ProfileForm() {
-  const [date, setDate] = useState<Date>();
+  const [open, setOpen] = useState(false);
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      fullname: "",
     },
   });
 
@@ -43,44 +54,65 @@ export function ProfileForm() {
   }
   return (
     <Form {...form}>
-      <form onSubmit={() => form.handleSubmit(onSubmit)} className="space-y-8">
+      <form
+        onSubmit={wrapAsyncFunction(form.handleSubmit(onSubmit))}
+        className="space-y-8"
+      >
         <FormField
           control={form.control}
-          name="username"
+          name="fullname"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>FullName</FormLabel>
               <FormControl>
                 <Input placeholder="shadcn" {...field} />
               </FormControl>
-              <FormLabel>Choose a date</FormLabel>
-              <FormControl>
-                <Popover>
-                  <PopoverTrigger asChild>
+
+              <FormDescription>Please enter your full name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Meeting Date</FormLabel>
+              <Popover open={open} onOpenChange={setOpen}>
+                <PopoverTrigger asChild>
+                  <FormControl>
                     <Button
                       variant={"outline"}
                       className={cn(
-                        "w-full justify-start text-left font-normal",
-                        !date && "text-muted-foreground"
+                        "pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
                       )}
                     >
-                      <CalendarIcon className="mr-2 h-4 w-4" />
-                      {date ? format(date, "PPP") : <span>Pick a date</span>}
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                     </Button>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0">
-                    <Calendar
-                      mode="single"
-                      selected={date}
-                      onSelect={setDate}
-                      initialFocus
-                    />
-                  </PopoverContent>
-                </Popover>
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={() => setOpen(false)}
+                    onDayClick={field.onChange}
+                    disabled={(date) =>
+                      date <
+                      new Date(new Date().setDate(new Date().getDate() - 1))
+                    }
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>Choose a date you want to meet.</FormDescription>
               <FormMessage />
             </FormItem>
           )}
