08c4cd9a1068d95822610a1a28419bb59f56a0e2
sachins602 <sachinsapkota4@gmail.com>
2023-07-05 02:10:02 +0545

    put next-auth secret

--- src/env.mjs
+++ src/env.mjs
@@ -9,10 +9,10 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
-    // NEXTAUTH_SECRET:
-    //   process.env.NODE_ENV === "production"
-    //     ? z.string().min(1)
-    //     : z.string().min(1).optional(),
+    NEXTAUTH_SECRET:
+      process.env.NODE_ENV === "production"
+        ? z.string().min(1)
+        : z.string().min(1).optional(),
     // NEXTAUTH_URL: z.preprocess(
     //   // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
     //   // Since NextAuth.js automatically uses the VERCEL_URL if present.
@@ -41,7 +41,7 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GMAIL_USER: process.env.GMAIL_USER,
     GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
